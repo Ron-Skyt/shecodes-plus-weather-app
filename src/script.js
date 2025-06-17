5ccb1822b7fafc7ab855f1f1146ca43a805b442f
Ron-Skyt <ronja.thoger@outlook.dk>
2025-06-17 19:31:58 +0200

    Added API to the forecast

--- src/script.js
+++ src/script.js
@@ -35,6 +35,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "tfafb400af42538491bd1a6a3a041o83";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -42,24 +49,27 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon"></div>
+  response.data.daily.forEach(function (day, index) {
+    if (index > 0 && index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div><img src="${
+      day.condition.icon_url
+    }" class="weather-forecast-icon"/></div>
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-temp-high">
-    °C</div>
-    <div class="weather-forecast-temp-low">°C</div> 
+    ▵${Math.round(day.temperature.maximum)}°C </div>
+    <div class="weather-forecast-temp-low">▿${Math.round(
+      day.temperature.minimum
+    )}°C</div> 
     </div>
     </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -117,53 +127,6 @@ function formatTimeAndDate() {
   let formattedMonth = months[month];
   currentDateElement.innerHTML = `${date.getDate()} ${formattedMonth}`;
 }
-
-/*
-let firstDayTemp = document.querySelector("#first-day-temp");
-let firstDayIcon = document.querySelector("#first-day-icon");
-firstDayIcon.innerHTML = `
-<img
-src="${response.data.daily[1].condition.icon_url}"
-class="current-weather-icon"
-/>`;
-let secondDayTemp = document.querySelector("#second-day-temp");
-let secondDayIcon = document.querySelector("#second-day-icon");
-secondDayIcon.innerHTML = `
-<img
-src="${response.data.daily[2].condition.icon_url}"
-class="current-weather-icon"
-/>`;
-let thirdDayTemp = document.querySelector("#third-day-temp");
-let thirdDayIcon = document.querySelector("#third-day-icon");
-thirdDayIcon.innerHTML = `
-<img
-src="${response.data.daily[3].condition.icon_url}"
-class="current-weather-icon"
-/>`;
-let fourthDayTemp = document.querySelector("#fourth-day-temp");
-let fourthDayIcon = document.querySelector("#fourth-day-icon");
-fourthDayIcon.innerHTML = `
-<img
-src="${response.data.daily[4].condition.icon_url}"
-class="current-weather-icon"
-/>`;
-let fifthDayTemp = document.querySelector("#fifth-day-temp");
-let fifthDayIcon = document.querySelector("#fifth-day-icon");
-fifthDayIcon.innerHTML = `
-<img
-src="${response.data.daily[2].condition.icon_url}"
-class="current-weather-icon"
-/>`;
-
-
-firstDayTemp.innerHTML = Math.round(response.data.daily[1].temperature.day);
-secondDayTemp.innerHTML = Math.round(response.data.daily[2].temperature.day);
-thirdDayTemp.innerHTML = Math.round(response.data.daily[3].temperature.day);
-fourthDayTemp.innerHTML = Math.round(response.data.daily[4].temperature.day);
-fifthDayTemp.innerHTML = Math.round(response.data.daily[5].temperature.day);
-
-console.log(response.data);
-*/
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
